b73a4e3fdc47d7a29f630377827b1d5f7b026b8e
Mitchell Henke <mitchell.henke@gsa.gov>
2022-10-18 15:46:12 -0500

    testing

--- app/services/encryption/small_aes_cipher.rb
+++ app/services/encryption/small_aes_cipher.rb
@@ -1,9 +1,5 @@
-# frozen_string_literal: true
-
 module Encryption
-  class AesCipher
-    include Encodable
-
+  class SmallAesCipher
     def encrypt(plaintext, cek)
       self.cipher = self.class.encryption_cipher
       # The key length for the AES-256-GCM cipher is fixed at 128 bits, or 32
@@ -34,7 +30,8 @@ module Encryption
       cipher.auth_data = 'PII'
       ciphertext = cipher.update(plaintext) << cipher.final
       tag = cipher.auth_tag
-      { iv: encode(iv), ciphertext: encode(ciphertext), tag: encode(tag) }.to_json
+
+      { iv: iv, ciphertext: ciphertext, tag: tag }.to_msgpack
     end
 
     def decipher(payload)
@@ -52,21 +49,21 @@ module Encryption
     end
 
     def unpack_payload(payload)
-      JSON.parse(payload, symbolize_names: true)
+      MessagePack.unpack(payload)
     rescue StandardError
       raise EncryptionError, 'Unable to parse encrypted payload'
     end
 
     def iv(unpacked_payload)
-      decode(unpacked_payload[:iv])
+      unpacked_payload['iv']
     end
 
     def tag(unpacked_payload)
-      decode(unpacked_payload[:tag])
+      unpacked_payload['tag']
     end
 
     def ciphertext(unpacked_payload)
-      decode(unpacked_payload[:ciphertext])
+      unpacked_payload['ciphertext']
     end
   end
 end
