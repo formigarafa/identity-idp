920ee474aaed11839a46f3fc98f0d149ca94fbcf
Sonia Connolly <sonia.connolly@gsa.gov>
2022-12-27 11:43:30 -0800

    Rename verify_pii_wait to verify_pii_wait_controller

--- app/controllers/idv/verify_pii_wait_controller.rb
+++ app/controllers/idv/verify_pii_wait_controller.rb
@@ -4,9 +4,22 @@ module Idv
 
     def show
       local_params = {
+        step_url: method(:idv_doc_auth_verify_pii_wait_url),
         step_template: "idv/doc_auth/verify_wait",
         flow_session: flow_session
       }
       render template: 'layouts/flow_step', locals: local_params
+    end
+
+    def create
+      redirect_to idv_review_url
+    end
+
+    # copied from doc_auth_controller
+    def flow_session
+      user_session['idv/doc_auth']
+    end
+
+
   end
 end
